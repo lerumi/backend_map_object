0cc29e9021a9a132cfb219f45fae6feccf321755
Lera <lerumi@inbox.ru>
2024-09-15 18:14:28 +0300

    database_connect

--- RIP1/settings.py
+++ RIP1/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'RIP1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'PORT': 8080,
+        'USER': 'postgres',
+        'PASSWORD': '235711',
+        'NAME': 'map_obj',
     }
 }
 
